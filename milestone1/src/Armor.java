402cc5fddbe762c8b049c4017e7fbf9bd40cdbc6
pmbennett <paulbennett001@gmail.com>
2021-11-19 21:06:15 -0800

    Javadoc added to new classes

--- milestone1/src/Armor.java
+++ milestone1/src/Armor.java
@@ -1,12 +1,56 @@
+/**
+ * Extends the product class, adding a "protection" field to represent the
+ * protection buff given by the armor. Chains Product constructor to allow for
+ * protection field to be initialized from constructor.
+ * 
+ * @author Paul Bennett
+ * @version 1.0
+ * @since 11/19/21
+ * 
+ * @see Product
+ */
 public class Armor extends Product {
 
-    private int protection; //add setter and getter 
-    public Armor(){
+    private int protection;
+
+    /**
+     * Default constructor.
+     */
+
+    public Armor() {
 
     }
 
-    public Armor(String name, String desc, double price, int qty, int protection){
+    /**
+     * Constructor with args to initialize instance variables on construction of the
+     * Product.
+     * 
+     * @param name       The name of the product.
+     * @param desc       A brief description of the product.
+     * @param price      The current sell price of the product.
+     * @param qty        The quantity of the item in stock at the storefront.
+     * @param protection The protection buff given by the armor.
+     */
+    public Armor(String name, String desc, double price, int qty, int protection) {
         super(name, desc, price, qty);
         this.protection = protection;
+    }
+
+    /**
+     * Sets the protection buff that the armor provides.
+     * 
+     * @param protection The protection buff given by the armor.
+     */
+    public void setProtection(int protection) {
+        this.protection = protection;
+    }
+
+    /**
+     * Returns the protection buff that the armor gives.
+     * 
+     * @return The protection buff given by the armor.
+     */
+    public int getProtection() {
+        return protection;
     }
 }
--- milestone1/src/Weapon.java
+++ milestone1/src/Weapon.java
@@ -1,12 +1,55 @@
+/**
+ * Extends the product class, adding a "damage" field to represent the damage
+ * given by the weapon. Chains Weapon constructor to allow for damage field to
+ * be initialized from constructor.
+ * 
+ * @author Paul Bennett
+ * @version 1.0
+ * @since 11/19/21
+ * 
+ * @see Product
+ */
 public class Weapon extends Product {
-    private int damage; //add setter and getter
 
+    private int damage;
+
+    /**
+     * Default constructor.
+     */
     public Weapon() {
 
     }
 
+    /**
+     * Constructor with args to initialize instance variables on construction of the
+     * Product.
+     * 
+     * @param name   The name of the product.
+     * @param desc   A brief description of the product.
+     * @param price  The current sell price of the product.
+     * @param qty    The quantity of the item in stock at the storefront.
+     * @param damage The damage the weapon deals.
+     */
     public Weapon(String name, String desc, double price, int qty, int damage) {
         super(name, desc, price, qty);
         this.damage = damage;
+    }
+
+    /**
+     * Sets damage per hit caused by the weapon.
+     * 
+     * @param damage The damage value for the weapon, per hit.
+     */
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    /**
+     * Returns damage value per hit.
+     * 
+     * @return Damage per hit.
+     */
+    public int getDamage() {
+        return damage;
     }
 }
