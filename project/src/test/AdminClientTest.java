7740ea551fa6529ced85e4801ca3fdbc1b5e1e4e
pmbennett <91107540+pmbennett@users.noreply.github.com>
2022-01-09 19:16:41 -0800

    moved libraries to lib folder updated JSON launch file updated JSON settings file to reflect dependencies

--- project/src/test/AdminClientTest.java
+++ project/src/test/AdminClientTest.java
@@ -1,6 +1,13 @@
 package test;
+
+import java.io.IOException;
+
 import org.junit.*;
 import main.*;
+
+/**
+ * This class tests the client side of the File Service admin applet.
+ */
 public class AdminClientTest {
-    
+
 }
